dfffee7b89f574d01c1977af72899803b9825622
Heet <heetzatakia123@gmail.com>
2023-03-05 12:07:29 +0530

    faq component

--- frontend/src/pages/Help.js
+++ frontend/src/pages/Help.js
@@ -1,168 +1,74 @@
-import React, { useEffect, useState } from "react";
-import { doc, onSnapshot, collection, query, where } from "firebase/firestore";
-import db from "./firebase";
-import { Video } from "./Video";
-import "./Help.css";
+import React, { useEffect, useState } from 'react';
+import { doc, onSnapshot, collection, query, where } from 'firebase/firestore';
+import db from './firebase';
+import { Video } from './Video';
+import './Help.css';
 import {
-	ref,
-	uploadBytesResumable,
-	getDownloadURL,
-	uploadBytes,
-	listAll,
-} from "firebase/storage";
-import { storage } from "./firebase";
-import { v4 } from "uuid";
-const videolistRef = ref(storage, "videos/");
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+  uploadBytes,
+  listAll,
+} from 'firebase/storage';
+import { storage } from './firebase';
+import { v4 } from 'uuid';
+const videolistRef = ref(storage, 'videos/');
 
 export const Help = () => {
-	const [reels, setReels] = useState([]);
-	const [file, setFile] = useState(null);
-	// const uploadImage = () => {
-	// 	if (file == null) return;
+  const [reels, setReels] = useState([]);
+  const [file, setFile] = useState(null);
 
-	// 	const fileRef = ref(storage, `videos/${file.name + v4()}`);
-	// 	uploadBytes(fileRef, file).then((snapshot) => {
-	// 		getDownloadURL(snapshot.ref).then((url) => {
-	// 			setvideoList((prev) => [...prev]);
-	// 			// console.log(video);
-	// 		});
-	// 		alert("Video Uploaded");
-	// 	});
-	// };
+  useEffect(() => {
+    const q = query(collection(db, 'reels'));
+    onSnapshot(q, (snapshot) => {
+      setReels(snapshot.docs.map((doc) => doc.data()));
+      console.log(snapshot.docs);
+    });
+    // console.log(reels);
+  }, []);
 
-	// const [video, setvideoList] = useState([]);
-
-	// useEffect(() => {
-	// 	listAll(videolistRef).then((response) => {
-	// 		response.items.forEach((item) => {
-	// 			getDownloadURL(item).then((url) => {
-	// 				// setvideoList((prev) => {
-	//                 //     const a = [...prev, url]
-	//                 //     setReels(a)
-	//                 //     return a
-
-	//                 // })
-
-	// 				// // console.log(video);
-	// 				// video.map((url) => {
-	// 				// 	setReels(url);
-	// 				// });
-	// 			});
-	// 		});
-	// 	});
-	// }, []);
-
-	useEffect(() => {
-		const q = query(collection(db, "reels"));
-		onSnapshot(q, (snapshot) => {
-			setReels(snapshot.docs.map((doc) => doc.data()));
-			console.log(snapshot.docs);
-		});
-		// console.log(reels);
-	}, []);
-
-	// const [file, setFile] = useState(""); // progress
-	// const [percent, setPercent] = useState(0); // Handle file upload event and update state
-	// function handleChange(event) {
-	// 	setFile(event.target.files[0]);
-	// }
-
-	// const handleUpload = () => {
-	// 	if (!file) {
-	// 		alert("Please upload an image first!");
-	// 	}
-
-	// 	const storageRef = ref(db, `/files/${file.name}`);
-	// 	const uploadTask = uploadBytesResumable(storageRef, file);
-
-	// 	uploadTask.on(
-	// 		"state_changed",
-	// 		(snapshot) => {
-	// 			const percent = Math.round(
-	// 				(snapshot.bytesTransferred / snapshot.totalBytes) * 100
-	// 			);
-
-	// 			setPercent(percent);
-	// 		},
-	// 		(err) => console.log(err),
-	// 		() => {
-	// 			getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-	// 				console.log(url);
-	// 			});
-	// 		}
-	// 	);
-	// };
-
-	return (
-		<>
-			<div className="trauma">
-				{/* <h1>Lets build A ig reels clone</h1> */}
-				<div
-					style={{ margin: "20px", paddingTop: "20px", textAlign: "center" }}
-				>
-					<h1 style={{ fontSize: "4em" }}>CAMPAIGNS</h1>
-				</div>
-			</div>
-			<div
-				style={{
-					display: "flex",
-					justifyContent: "space-evenly",
-					alignItems: "center",
-					flexDirection: "row",
-					maxWidth: "80%",
-					margin: "auto",
-				}}
-			>
-				<div style={{ maxWidth: "50%" }}>
-					<h2 style={{ fontSize: "3em" }}>
-						Deliver your message with{" "}
-						<span
-							style={{
-								fontSize: "1.5em",
-								fontWeight: "600",
-								color: "#00e78d ",
-							}}
-						>
-							POWER.
-						</span>{" "}
-						<br></br> Show your strengths and{" "}
-						<span
-							style={{ fontSize: "1em", fontWeight: "600", marginTop: "10px" }}
-						>
-							empower the future.
-						</span>
-					</h2>
-				</div>
-				<div className="app_videos">
-					{reels.map(({ url }) => (
-						<Video url={url} />
-					))}
-				</div>
-			</div>
-			{/* <div>
-				<input
-					type="file"
-					onChange={(event) => {
-						setFile(event.target.files[0]);
-					}}
-				/>
-				<button onClick={uploadImage}>Upload Image</button>
-			</div> */}
-			{/* <div>
-				{video.map((url) => {
-					return <video src={url} autoPlay />;
-				})}
-			</div> */}
-			{/* <div className="app_videos">
-				{video.map((url) => (
-					<Video url={url} />
-				))}
-			</div> */}
-			{/* <div>
-				<input type="file" onChange={handleChange} accept="/image/*" />
-				<button onClick={handleUpload}>Upload to Firebase</button>
-				<p>{percent} "% done"</p>
-			</div> */}
-		</>
-	);
+  return (
+    <>
+      <div className="trauma">
+        {/* <h1>Lets build A ig reels clone</h1> */}
+        <div
+          style={{ margin: '20px', paddingTop: '20px', textAlign: 'center' }}>
+          <h1 style={{ fontSize: '4em' }}>CAMPAIGNS</h1>
+        </div>
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          alignItems: 'center',
+          flexDirection: 'row',
+          maxWidth: '80%',
+          margin: 'auto',
+        }}>
+        <div style={{ maxWidth: '50%' }}>
+          <h2 style={{ fontSize: '3em' }}>
+            Deliver your message with{' '}
+            <span
+              style={{
+                fontSize: '1.5em',
+                fontWeight: '600',
+                color: '#00e78d ',
+              }}>
+              POWER.
+            </span>{' '}
+            <br></br> Show your strengths and{' '}
+            <span
+              style={{ fontSize: '1em', fontWeight: '600', marginTop: '10px' }}>
+              empower the future.
+            </span>
+          </h2>
+        </div>
+        <div className="app_videos">
+          {reels.map(({ url }) => (
+            <Video url={url} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
--- frontend/src/pages/HelpMe.js
+++ frontend/src/pages/HelpMe.js
@@ -1,92 +1,92 @@
-import React, { useEffect, useState } from "react";
-import { doc, onSnapshot, collection, query, where } from "firebase/firestore";
-import db from "./firebase";
-import { Video } from "./Video";
-import "./HelpMe.css";
-import Faq from "react-faq-component";
-import Navbar from "../components/Navbar";
+import React, { useEffect, useState } from 'react';
+import { doc, onSnapshot, collection, query, where } from 'firebase/firestore';
+import db from './firebase';
+import { Video } from './Video';
+import './HelpMe.css';
+import Faq from 'react-faq-component';
+import Navbar from '../components/Navbar';
 
 const data = {
-	title: "FAQS",
-	rows: [
-		{
-			title: "What is the app about?",
-			content: `A blockchainbased
+  title: 'FAQS',
+  rows: [
+    {
+      title: 'What is the app about?',
+      content: `A blockchainbased
       platform for decentralized governance that enables stakeholders to vote and
       make decisions in a secure and transparent manner can address these issues and
       promote trust and accountability in the voting process.`,
-		},
-		{
-			title: "Is it free?",
-			content: "",
-		},
-		{
-			title: "Where is blockchain being used in the app?",
-			content: `Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc, ac sagittis leo elit vel lorem.
-          Fusce tempor lacus ut libero posuere viverra. Nunc velit dolor, tincidunt at varius vel, laoreet vel quam.
-          Sed dolor urna, lobortis in arcu auctor, tincidunt mattis ante. Vivamus venenatis ultricies nibh in volutpat.
-          Cras eu metus quis leo vestibulum feugiat nec sagittis lacus.Mauris vulputate arcu sed massa euismod dignissim. `,
-		},
-	],
+    },
+    {
+      title: 'Is it free?',
+      content:
+        'Our services will be free for government services and government use while for personal and businness plans we actively have a freemium model that will be ',
+    },
+    {
+      title: 'Where is blockchain being used in the app?',
+      content: `Blockchain here is used to  `,
+    },
+  ],
 };
 
 const styles = {
-	// bgColor: 'white',
-	titleTextColor: "blue",
-	rowTitleColor: "blue",
-	// rowContentColor: 'grey',
-	// arrowColor: "red",
+  bgColor: '#121113',
+  titleTextColor: '#6bcadb',
+  rowTitleColor: '#6bcadb',
+  rowContentColor: '#6bcadb',
+  arrowColor: '#6bcadb',
+  rowContentPaddingRight: '150px',
+  rowContentPaddingLeft: '10px',
+  rowContentPaddingTop: '10px',
+  rowContentPaddingBottom: '10px',
 };
 
 const config = {
-	animate: true,
-	// arrowIcon: "V",
-	tabFocus: true,
+  animate: true,
+  // arrowIcon: "V",
+  tabFocus: true,
 };
 
 export const HelpMe = () => {
-	const [reels, setReels] = useState([]);
+  const [reels, setReels] = useState([]);
 
-	useEffect(() => {
-		const q = query(collection(db, "learn"));
-		onSnapshot(q, (snapshot) => {
-			setReels(snapshot.docs.map((doc) => doc.data()));
-			console.log(snapshot.docs);
-		});
-		// console.log(reels);
-	}, []);
+  useEffect(() => {
+    const q = query(collection(db, 'learn'));
+    onSnapshot(q, (snapshot) => {
+      setReels(snapshot.docs.map((doc) => doc.data()));
+      console.log(snapshot.docs);
+    });
+    // console.log(reels);
+  }, []);
 
-	return (
-		<>
-			<Navbar />
-			<div className="card" style={{ backgroundColor: "black" }}>
-				{/* <h1>Lets build A ig reels clone</h1> */}
-				<div
-					style={{ margin: "20px", paddingTop: "20px", textAlign: "center" }}
-				>
-					<h1 style={{ fontSize: "4em" }}>Quick overview </h1>
-					<p style={{ color: "white" }}>
-						To give you an idea of what the app is about.
-					</p>
-				</div>
-			</div>
-			<div
-				style={{
-					display: "flex",
-					justifyContent: "center",
-					backgroundColor: "black",
-					height: "100vh",
-				}}
-			>
-				<div className="videos">
-					{reels.map(({ url }) => (
-						<Video url={url} />
-					))}
-				</div>
-			</div>
-			<div>
-				<Faq data={data} styles={styles} config={config} />
-			</div>
-		</>
-	);
+  return (
+    <div style={{ backgroundColor: '#121113' }}>
+      <Navbar />
+      <div className="card" style={{ backgroundColor: '#121113' }}>
+        {/* <h1>Lets build A ig reels clone</h1> */}
+        <div
+          style={{ margin: '20px', paddingTop: '20px', textAlign: 'center' }}>
+          <h1 style={{ fontSize: '4em' }}>Quick overview </h1>
+          <p style={{ color: 'white' }}>
+            To give you an idea of what the app is about.
+          </p>
+        </div>
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          backgroundColor: '#121113',
+          height: '100vh',
+        }}>
+        <div className="videos">
+          {reels.map(({ url }) => (
+            <Video url={url} />
+          ))}
+        </div>
+      </div>
+      <div style={{ width: '80%', margin: '-6em auto' }}>
+        <Faq data={data} styles={styles} config={config} />
+      </div>
+    </div>
+  );
 };
